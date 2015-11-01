fcbe767bf157c6d0e47d0472c0294d1dedb05335
Gean Carlos <wochap@users.noreply.github.com>
2015-11-01 14:12:16 -0500

    [update] Initializr

--- views/landing/partials/layout_main.blade.php
+++ views/landing/partials/layout_main.blade.php
@@ -2,11 +2,11 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <meta name="description" content=""/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-    <title>Aupair Exchange</title>
+    <title>Boilerplate</title>
 
     <!-- og tags Facebook -->
     <!-- /og tags Facebook -->
@@ -20,8 +20,14 @@
     <link rel="icon" href="#.png" sizes="192x192"/>
     <!-- Stylesheets -->
     <link rel="Stylesheet" type="text/css" href="{{ asset('landing/css/main.css') }}"/>
+    
+    <script src="{{ asset('landing/js/modernizr.js') }}"></script>
 </head>
 <body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+        
     @include('landing.partials.header')
 
     <!-- Content -->
@@ -33,11 +39,23 @@
     @include('landing.partials.footer')
 
     <!-- Scripts -->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script type="text/javascript" src="landing/js/main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="{{ asset("landing/js/jquery.js") }}"><\/script>')</script>
+    <script src="{{ asset('landing/js/main.js') }}"></script>
     @yield('scripts')
     <!-- /Scripts -->
-
+    
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script async type="text/javascript">
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+    <!-- End Google Analytics -->
+    
     <!-- Live Reload -->
     <!-- <script src="http://localhost:35729/livereload.js?snipver=1" type="text/javascript"></script> -->
     <!-- /Live Reload -->
