b4a3352e53b43538de0c3706eb28691a2e527c98
Gean Carlos <wochap@users.noreply.github.com>
2015-11-03 15:29:03 -0500

    [add] mainMenu

---
+++ views/landing/partials/nav.blade.php
@@ -0,0 +1,19 @@
+<nav class="mainMenu">
+    <ul class="mainMenu-list">
+        <li class="mainMenu-item">
+            <a href="#" class="mainMenu-link mainMenu-link--active">Nosotros</a>
+        </li>
+        <li class="mainMenu-item">
+            <a href="#" class="mainMenu-link">Programas</a>
+        </li>
+        <li class="mainMenu-item">
+            <a href="#" class="mainMenu-link">Servicios</a>
+        </li>
+        <li class="mainMenu-item">
+            <a href="#" class="mainMenu-link">Experiencias</a>
+        </li>
+        <li class="mainMenu-item">
+            <a href="#" class="mainMenu-link">Contáctanos</a>
+        </li>
+    </ul>
+</nav>
