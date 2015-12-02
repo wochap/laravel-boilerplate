8e1e5af2b15ecda5e1bce9b6c1b7f85cb1ec8a80
Gean Carlos <wochap@users.noreply.github.com>
2015-12-02 12:25:31 -0500

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,28 +1,55 @@
-if ($("#slider-portada").length) {
-    $("#slider-portada").owlCarousel({
-        autoPlay: true,
-        pagination: true,
-        singleItem: true,
-        transitionStyle: "fade"
-    });
-}
+(function(){
+    if ($("#slider-capacitacion").length) {
+        if ($(window).width() <= '768') {
+            $("#slider-capacitacion").owlCarousel({
+                autoPlay: true,
+                navigation : true,
+                navigationText: ["<i class='icon-arrow-left'></i>","<i class='icon-arrow-right'></i>"],
+                singleItem: true
+            });
+        }
+    }
 
-if ($("#slider-capacitacion").length) {
-    if ($(window).width() <= '768') {
-        $("#slider-capacitacion").owlCarousel({
-            autoPlay: true,
-            navigation : true,
-            navigationText: ["<i class='icon-arrow-left'></i>","<i class='icon-arrow-right'></i>"],
-            singleItem: true
+    if ($("#carousel-concepto").length) {
+        $("#carousel-concepto").owlCarousel({
+            autoPlay: 3500,
+            pagination: true,
+            navigation: false,
+            singleItem: true,
+            transitionStyle: "fade"
         });
     }
-}
 
-if ($("#slider-testimonio").length) {
-    $("#slider-testimonio").owlCarousel({
-        autoPlay: true,
-        pagination: true,
-        singleItem: true,
-        transitionStyle: "fade"
-    });
-}
+    if ($("#carousel-portada").length) {
+        $("#carousel-portada").owlCarousel({
+            autoPlay: true,
+            pagination: false,
+            navigation: false,
+            singleItem: true,
+            transitionStyle: "fade"
+        });
+    }
+
+    if (html.classList.contains("mobil")) {
+        if ($("#carousel-ofrecemos").length) {
+            $("#carousel-ofrecemos").owlCarousel({
+                autoPlay: 3000,
+                singleItem: true,
+                pagination: false,
+                navigation: false
+            });
+        }
+
+        if ($("#carousel-subscripcion").length) {
+            $("#carousel-subscripcion").owlCarousel({
+                autoPlay: 3000,
+                singleItem: true,
+                pagination: false,
+                navigation: false
+            });
+        }
+    }
+})();
+
+
+
