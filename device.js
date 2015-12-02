c9b6f4fd9bca5db88bcf90da0ed0a6cd8375dea8
Gean Carlos <wochap@users.noreply.github.com>
2015-12-02 12:20:50 -0500

    Update device.js

--- device.js
+++ device.js
@@ -3,3 +3,12 @@ if ($(window).width() <= '768') {
 } else {
     $('html').addClass('desktop');
 }
+
+
+// añade clases si es mobil o desktop
+var html = document.getElementsByTagName("html")[0];
+if (window.innerWidth  <= '480') {
+    html.classList.add("mobil");
+} else {
+    html.classList.add("desktop");
+}
